49182be8003f4ada5ee2a8e80eae1a2ea334241f
senlinjun <yu2755221817@163.com>
2023-06-23 18:59:48 +0800

    1.将连接互联网获取版本信息改为从本地获取

--- src/lib.py
+++ src/lib.py
@@ -1,6 +1,4 @@
-import os
-from requests import get
-from lxml import etree
+import os,json
 from PyQt5 import QtWidgets
 
 
@@ -15,30 +13,8 @@ def buildDirectories(path, directories):
 
 
 def getBedrockGameVersionsList():
-    g = get(
-        "https://minecraft.fandom.com/zh/wiki/%E5%9F%BA%E5%B2%A9%E7%89%88%E7%89%88%E6%9C%AC%E8%AE%B0%E5%BD%95"
-    )
-    html = etree.HTML(g.text)
-    main_versions_list = html.xpath(
-        '//div[@class="mw-parser-output"]/h3/span[@class="mw-headline"]/text()'
-    )
-    detailed_versions_list = html.xpath(
-        '//div[@class="mw-parser-output"]/table[@class="wikitable"]/tbody/tr/td[1]/a/text()'
-    )
-    return_dict = {}
-    for version in main_versions_list:
-        if version >= "1.0" and version != "Pre-Release":
-            return_dict[version] = []
-    for version in detailed_versions_list:
-        version_list = version.split(".")
-        if len(version_list) != 3:
-            continue
-        main_version = f"{version_list[0]}.{version_list[1]}"
-        if main_version in main_versions_list:
-            return_dict[main_version].append(version)
-    for version in return_dict:
-        return_dict[version].append(f"{version}.0")
-
+    with open("./data/versions.json","r") as f:
+        return_dict = json.load(f)
     return return_dict
 
 
